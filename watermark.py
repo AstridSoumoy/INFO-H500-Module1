f3fb2958b7fe892daeb7d16b1edd2b482a8b3cf0
Astrid <astrid.soumoy@polymtl.ca>
2020-11-05 22:05:42 +0100

    Init code and test it

--- watermark.py
+++
@@ -1,8 +0,0 @@
-from skimage.io import imread,imsave,imshow
-%matplotlib inline
-
-watermarkImg = imread('watermark.png')
-photography = imread('road.jpg')
-print('test')
-imshow(photography)
-imsave('result.jpg', photography)
